e36e81155be90064a73cfb18e7b631eaef8e68c9
guoyunrui11@gmail.com <guoyunrui11@gmail.com>
2021-02-16 15:44:15 +0100

    sh

---
+++ train.sh
@@ -0,0 +1,7 @@
+
+
+python lvae_train.py --dataset=tiny_imagenet --split=split0 --gpu_id=0
+
+python lvae_train.py --dataset=tiny_imagenet --split=split1 --gpu_id=0
+
+python lvae_train.py --dataset=tiny_imagenet --split=split2 --gpu_id=0
---
+++ train_gpu1.sh
@@ -0,0 +1,5 @@
+
+
+python lvae_train.py --dataset=tiny_imagenet --split=split3 --gpu_id=1
+
+python lvae_train.py --dataset=tiny_imagenet --split=split4 --gpu_id=1
